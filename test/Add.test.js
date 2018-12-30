94b64746983ce5a7fa3503b6e3abdd02fa66e56c
wasuradananjith <wasuradananjith@gmail.com>
2018-12-30 23:33:10 +0530

    remove beforeEach method from Add.test.js

--- test/Add.test.js
+++ test/Add.test.js
@@ -1,13 +1,8 @@
 import Add from '../src/components/Add';
 
-let wrapper;
-
-beforeEach(() => {
-    wrapper = shallow(<Add />);
-});
-
 describe('<Comment /> rendering', () => {
     it('should render one <h1>', () => {
+        let wrapper = shallow(<Add />);
         expect(wrapper.children('h1')).toHaveLength(1);
     });
 });
